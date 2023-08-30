f78732835e9d40d4c22315307e66e15a7b4a9632
Semeniaka Kateryna <ekaterina.semeniaka@gmail.com>
2023-08-30 23:39:29 +0300

    - added usage of the config.properties file - added method for database creation - added method for checking database existence

--- core/src/main/java/com/core/database_operations/DatabaseOperations.java
+++ core/src/main/java/com/core/database_operations/DatabaseOperations.java
@@ -1,14 +1,21 @@
 package com.core.database_operations;
 
+import com.core.configuration.ConfigurationManager;
 import org.testng.Assert;
 
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class DatabaseOperations {
+    public DatabaseOperations() throws IOException {
+    }
 
-    private static final String jdbcUrl = "jdbc:mysql://host.docker.internal:3306/jdbc_test_database";
-    private static final String user = "root";
-    private static final String password = "qweqwe12";
+    private Properties properties = ConfigurationManager.loadProperties();
+    private String jdbcUrl = properties.getProperty("jdbc.url");
+    private String user = properties.getProperty("jdbc.user");
+    private String password = properties.getProperty("jdbc.password");
+
     private Connection connection;
 
 
@@ -22,13 +29,20 @@ public class DatabaseOperations {
         while (resultSet.next()) {
             rowCount++;
         }
-        Assert.assertEquals(rowCount, expRowCount,  String.format("Expected %d rows in the %s table", expRowCount, tableName));
+        Assert.assertEquals(rowCount, expRowCount, String.format("Expected %d rows in the %s table", expRowCount, tableName));
         return this;
     }
 
     public DatabaseOperations verifyTableExists(String tableName) throws SQLException {
         boolean tableExists = checkIfTableExists(tableName);
         Assert.assertTrue(tableExists, "Table " + tableName + " doesn't exist.");
+        return this;
+    }
+
+    public DatabaseOperations createDatabase(String databaseName) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("CREATE DATABASE " + databaseName);
+        Assert.assertTrue(checkIfDatabaseExists(databaseName));
         return this;
     }
 
@@ -49,7 +63,7 @@ public class DatabaseOperations {
         }
     }
 
-    private  ResultSet executeSelectQuery(String tableName) throws SQLException {
+    private ResultSet executeSelectQuery(String tableName) throws SQLException {
         String selectTableQuery = "SELECT * FROM " + tableName;
         Statement statement = connection.createStatement();
         return statement.executeQuery(selectTableQuery);
@@ -58,6 +72,25 @@ public class DatabaseOperations {
     private boolean checkIfTableExists(String tableName) throws SQLException {
         ResultSet resultSet = connection.getMetaData().getTables(null, null, tableName, null);
         return resultSet.next();
+    }
+
+    private boolean checkIfDatabaseExists(String databaseName) throws SQLException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet resultSet = null;
+        try {
+            resultSet = metaData.getCatalogs();
+            while (resultSet.next()) {
+                String existingDatabaseName = resultSet.getString("TABLE_CAT");
+                if (databaseName.equalsIgnoreCase(existingDatabaseName)) {
+                    return true;
+                }
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
     }
 
     public void closeConnection() throws SQLException {
